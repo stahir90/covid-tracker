e667553e1bc4fae4bcdf44d4dba26b817872f31b
Tahir Mahmood <stahir.net@gmail.com>
2025-03-06 05:00:23 +1100

    route update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,26 @@ import { Routes } from '@angular/router';
 import { StateViewComponent } from './state-view/state-view.component';
 import { DistrictViewComponent } from './district-view/district-view.component';
 
+// Define the list of all possible state names
+const stateNames = ['StateA', 'StateB', 'StateC', 'StateD'];
+
+// Function to provide prerender parameters
+export function getPrerenderParams() {
+  return stateNames.map((stateName) => ({ stateName }));
+}
+
 export const routes: Routes = [
   { path: 'state', component: StateViewComponent },
-  { path: 'state/:stateName', component: DistrictViewComponent },
+  {
+    path: 'state/:stateName',
+    component: DistrictViewComponent,
+    providers: [
+      {
+        provide: 'getPrerenderParams',
+        useValue: getPrerenderParams,
+      },
+    ],
+  },
   { path: '', redirectTo: '/state', pathMatch: 'full' },
   { path: '**', redirectTo: '/state' },
 ];
